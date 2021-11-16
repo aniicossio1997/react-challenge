3467371a4f18578f0841a48384e307c0a546eca9
Ana Cossio <anacossio7@gmail.com>
2021-11-16 15:54:42 -0300

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import "./assets/css/App.css";
-import {AuthProvider} from "./context/AuthContext";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NotFound from "./pages/NotFound";
-import Navbar from "./layout/Navbar"
-import Footer from "./layout/Footer"
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 import Show from "./components/team/Show";
 import ProtectedRoute from "./ProtectedRoute";
 import WithoutProted from "./WithoutProted";
@@ -15,33 +11,31 @@ import FormHero from "./components/team/addHero/FormHero";
 import PageLogin from "./pages/PageLogin";
 import PageTeam from "./pages/PageTeam";
 import { Provider } from "react-redux";
-import generateStore from "./redux/store"
+import generateStore from "./redux/store";
 function App(): JSX.Element {
-  const store = generateStore()
+  const store = generateStore();
 
   return (
     <>
       <AuthProvider>
-
-      <Provider store={store}>
-        <Router>
-          <Navbar/>
+        <Provider store={store}>
+          <Router>
+            <Navbar />
 
             <div className="main container-width mt-2">
-
               <Switch>
-              <ProtectedRoute path="/team/add" component={FormHero} />
-              <ProtectedRoute path="/team/:id" component={Show}  exact />
-              <ProtectedRoute path="/team" component={PageTeam} />
-              <WithoutProted path="/login" component={PageLogin}  exact/>
-              <WithoutProted path="/" component={PageLogin}  exact/>
-              <Route path="/:rest*"><NotFound/></Route>
+                <ProtectedRoute path="/team/add" component={FormHero} />
+                <ProtectedRoute path="/team/:id" component={Show} exact />
+                <ProtectedRoute path="/team" component={PageTeam} />
+                <WithoutProted path="/login" component={PageLogin} exact />
+                <WithoutProted path="/" component={PageLogin} exact />
+                <Route path="/:rest*">
+                  <NotFound />
+                </Route>
               </Switch>
-
-
             </div>
-          <Footer/>
-        </Router>
+            <Footer />
+          </Router>
         </Provider>
       </AuthProvider>
     </>
